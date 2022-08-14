5a725810b3e3e2c17f9ec2bd61814f7f45fbf121
adamiaonr <adamiaonr@gmail.com>
2022-08-14 14:32:25 +0100

    added setup.py file to install src module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='src',
+   version='1.0',
+   description='module for MLOps Zoomcamp project, 2022 edition',
+   author='Antonio Rodrigues',
+   author_email='adamiaonr@gmail.com',
+   packages=['src'],  #same as name
+)

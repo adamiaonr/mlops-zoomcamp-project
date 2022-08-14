d2c7e8492f068cc2943ac81fd262a3ba528e5ddf
adamiaonr <adamiaonr@gmail.com>
2022-08-14 14:31:24 +0100

    added utility functions to src module

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import pickle
+
+
+def dump_pickle(obj, filename):
+    with open(filename, "wb") as f_out:
+        return pickle.dump(obj, f_out)
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
